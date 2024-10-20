d4a5972fb5b1633f65b52be5125741822e945b95
Neirel <neirelzapatos@gmail.com>
2024-10-19 18:18:39 -0700

    integrated mongodb

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import schema from './schema';
+import dbConnect from '@/app/lib/dbConnect';
+import Product from '@/app/models/Product';
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Milk', price: 2.5},
-        { id: 2, name: 'Eggs', price: 4}
-    ]);
+// export function GET(request: NextRequest) {
+//     return NextResponse.json([
+//         { id: 1, name: 'Milk', price: 2.5},
+//         { id: 2, name: 'Eggs', price: 4}
+//     ]);
+// }
+
+export default async function GET(){
+    await dbConnect();
+
+    try {
+        const products = await Product.find({});
+        return NextResponse.json(products);
+    } catch (err: any) {
+        return NextResponse.json({ error: err.message })
+    }
 }
 
 export async function POST(request: NextRequest) {
